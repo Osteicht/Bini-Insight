03e8c31d3c6efdc734f78e5866a1c80b683d84d5
CodeBuddySLTNS <renz2arjhay@gmail.com>
2024-07-12 21:48:59 +0800

    codebuddy(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠)◜⁠✧

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require(`express`);
-const port = process.env.port || 5000;
+const path = require('path');
+const router = require('./routes/router');
+  
 const app = express();
+const port = process.env.port || 5000;
 
-const homeRoute = require(`./route/homepage`);
 
-app.use(`/`, homeRoute);
-app.use(`/style.css`, homeRoute);
-app.use(`/javascript.js`, homeRoute);
-app.use(`/home/`, homeRoute);
+// middlewares
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);

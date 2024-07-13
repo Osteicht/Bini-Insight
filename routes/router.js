8c1013bb4dcfa04285cd501380fd2a59bba6d784
CodeBuddySLTNS <renz2arjhay@gmail.com>
2024-07-13 18:26:48 +0800

    initial commit ni codebuddy

--- routes/router.js
+++ routes/router.js
@@ -6,4 +6,8 @@ router.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "about.html"));
 })
 
+router.get("/articles", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "articles.html"));
+})
+
 module.exports = router;
